4890d5346de7b0f64de1982387959ba04338841a
Anton Tur <ant.tur85@gmail.com>
2023-01-07 21:18:14 +0200

    add 2 section and menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,17 @@
-$(function () {
+const hamburger = document.querySelector('.hamburger'),
+	menu = document.querySelector('.menu'),
+	closeElem = document.querySelector('.menu__close'),
+	overlay = document.querySelector('.menu__overlay');
 
-})
+hamburger.addEventListener('click', () => {
+	menu.classList.add('active');
+});
+
+function closeMenu(elem) {
+	elem.addEventListener('click', () => {
+		menu.classList.remove('active');
+	});
+}
+
+closeMenu(closeElem);
+closeMenu(overlay);
